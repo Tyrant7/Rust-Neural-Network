b5427d21a0cab5466ef452ce31dd6fcff9f47a94
Carson <carsonburke22@gmail.com>
2024-12-06 22:41:47 -0800

    remove and update depends

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,7 @@
 
 use std::{vec, collections::HashMap, borrow::Borrow, time::{Duration, Instant}};
 
-use eventual::Timer;
 use neural_network::{InputName, OutputName};
-extern crate eventual;
 
 use std::sync::mpsc::Sender;
 
@@ -36,7 +34,7 @@ fn main() {
     ];
     
     let mut neural_network = init(&inputs, outputs.len());
-    tick_manager(&mut neural_network, inputs);
+    // tick_manager(&mut neural_network, inputs);
     
     println!("End");
 }
@@ -49,31 +47,31 @@ pub fn init(inputs: &Vec<Input>, output_count: usize) -> NeuralNetwork {
     neural_network
 }
 
-pub fn tick_manager(neural_network: &mut NeuralNetwork, inputs: Vec<Input>) {
+// pub fn tick_manager(neural_network: &mut NeuralNetwork, inputs: Vec<Input>) {
 
-    let time_start = Instant::now();
+//     let time_start = Instant::now();
 
-    let timer = Timer::new();
-    let ticks = timer.interval_ms(TICK_SPEED).iter();
+//     let timer = Timer::new();
+//     let ticks = timer.interval_ms(TICK_SPEED).iter();
     
-    for (tick, _) in ticks.enumerate() {
+//     for (tick, _) in ticks.enumerate() {
 
-        if tick > 500 {
-            break;
-        }
+//         if tick > 500 {
+//             break;
+//         }
 
-        print!("Processing tick: ");
-        println!("{}", tick);
+//         print!("Processing tick: ");
+//         println!("{}", tick);
 
-        let time_elapsed = time_start.elapsed();
-        println!("{:?}", time_elapsed);
+//         let time_elapsed = time_start.elapsed();
+//         println!("{:?}", time_elapsed);
 
-        neural_network.forward_propagate(&inputs);
+//         neural_network.forward_propagate(&inputs);
 
-        if tick % 10 == 0 {
+//         if tick % 10 == 0 {
 
-            neural_network.mutate();
-            neural_network.write_to_file();
-        }
-    }
-}
+//             neural_network.mutate();
+//             neural_network.write_to_file();
+//         }
+//     }
+// }
