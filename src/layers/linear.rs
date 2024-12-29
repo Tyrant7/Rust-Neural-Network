86ebde99398c7f38e83f52f858ae2cbfa0cc5220
Tyrant <tyler.sauve@bag-of-holding.com>
2024-12-29 10:40:59 -0800

    Began implementing

--- src/layers/linear.rs
+++ src/layers/linear.rs
@@ -1,7 +1,27 @@
 use crate::layer::{Layer, LayerShape};
 
-pub struct Linear;
+pub struct Linear {
+    shape: LayerShape,
+    weights: Vec<Vec<f32>>,
+    bias: Vec<f32>,
+}
+
+impl Linear {
+    pub fn new(input_shape: u32, output_shape: u32) -> Self {
+        Linear {
+            shape: LayerShape::new(input_shape, output_shape),
+            weights: vec![vec![0.; input_shape as usize]; output_shape as usize],
+            bias: vec![0.; output_shape as usize],
+        }
+    }
+}
 
 impl Layer for Linear {
+    fn forward(&self, input: Vec<f32>) -> Vec<f32> {
+        input // TODO
+    }
 
+    fn backward(&self) -> Vec<f32> {
+        vec![] // TODO
+    }
 }
--- src/layers/relu.rs
+++ src/layers/relu.rs
@@ -4,12 +4,20 @@ pub struct ReLU {
     shape: LayerShape,
 }
 
+impl ReLU {
+    pub fn new(input_shape: u32, output_shape: u32) -> Self {
+        ReLU {
+            shape: LayerShape::new(input_shape, output_shape),
+        }
+    }
+}
+
 impl Layer for ReLU {
     fn forward(&self, input: Vec<f32>) -> Vec<f32> {
         input.into_iter().map(|x| x.max(0.)).collect()
     }
 
     fn backward(&self) -> Vec<f32> {
-        
+        vec![] // TODO
     }
 }
