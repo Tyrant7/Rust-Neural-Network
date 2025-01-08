b8e2a6c4963f9ed6912e9f32c4a64b1fa23d5009
Carson <carsonburke22@gmail.com>
2025-01-07 16:16:13 -0800

    leaky relu activation function

--- src/activation_functions.rs
+++ src/activation_functions.rs
@@ -4,6 +4,7 @@ pub enum ActivationFunction {
     None,
     ReLU,
     Sigmoid,
+    LeakyReLU
 }
 
 impl ActivationFunction {
@@ -12,6 +13,10 @@ impl ActivationFunction {
             ActivationFunction::None => input,
             ActivationFunction::ReLU => input.mapv(|x| x.max(0.)),
             ActivationFunction::Sigmoid => input.mapv(sigmoid_internal),
+            ActivationFunction::LeakyReLU => input.mapv(|x| match x < 0. {
+                true => 0.01 * x,
+                false => x
+            }),
         }
     }
 
@@ -20,6 +25,10 @@ impl ActivationFunction {
             ActivationFunction::None => input,
             ActivationFunction::ReLU => input.mapv(|x| if x > 0. { 1. } else { 0. }),
             ActivationFunction::Sigmoid => input.mapv(|x| sigmoid_internal(x) * (1. - sigmoid_internal(x))),
+            ActivationFunction::LeakyReLU => input.mapv(|x| match x < 0. {
+                true => 0.01,
+                false => 1.
+            })
         }
     }
 }
